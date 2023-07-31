e5f6ece98ec0472013a71d0493bf812a60f5b160
coded-dolphin <nepalsarbagya69@gmail.com>
2023-07-31 23:16:03 +0545

    ADDING BUNCH OF STUFF

---
+++ app/script.js
@@ -0,0 +1,19 @@
+const rtl1 = document.getElementById("rtl1")
+const rtl2 = document.getElementById("rtl2")
+const rtl3 = document.getElementById("rtl3")
+
+function rtl1_clk1(){
+    rtl1.style.zIndex = "1"
+    rtl2.style.zIndex = "0"
+    rtl3.style.zIndex = "0"
+}
+function rtl2_clk2(){
+    rtl2.style.zIndex = "1"
+    rtl3.style.zIndex = "0"
+    rtl1.style.zIndex = "0"
+}
+function rtl3_clk3(){
+    rtl3.style.zIndex = "1"
+    rtl1.style.zIndex = "0"
+    rtl2.style.zIndex = "0"
+}
