8e57e1a86a62d4ec81b82105c45d21f977427933
coded-dolphin <nepalsarbagya69@gmail.com>
2023-08-08 09:25:13 +0545

    adding everything

--- README.md
+++
@@ -1,1 +0,0 @@
-# coded-dolphin.github.io
--- app/script.js
+++
@@ -1,7 +0,0 @@
-let highestZIndex = 1; // Initialize the highest z-index
-
-function bringToFront(element) {
-  // Increment the highestZIndex and set it as the new z-index for the clicked element
-  highestZIndex++;
-  element.style.zIndex = highestZIndex;
-}
